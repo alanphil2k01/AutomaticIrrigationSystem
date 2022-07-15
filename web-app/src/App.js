5f556db3abf2ff899f4653d6998eecd72d9f7d1f
Alan <alanphilipose@gmail.com>
2022-07-16 01:01:28 +0530

    initial web-app, connect to firebase and get soil data

---
+++ web-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { database } from './firebase/firebase';
+import { useEffect, useState } from 'react';
+import { Database, onValue, ref } from 'firebase/database';
+
+function App() {
+
+    const [soilData, setSoilData] = useState();
+    useEffect(() => {
+        const querySoil = ref(database,  "A123-12387876123873288123/soil");
+        return onValue(querySoil, (snapshot) => {
+            if (snapshot.exists()) {
+                setSoilData(snapshot.val());
+            }
+        });
+    });
+
+    return (
+        <div className="App">
+            <p>Soil moisture = { soilData }%</p>
+        </div>
+    );
+}
+
+export default App;
