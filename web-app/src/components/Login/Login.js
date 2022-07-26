c21add7dff098bffa62058943ea52f663c334de4
21vishnuvinod <67964495+21vishnuvinod@users.noreply.github.com>
2022-07-26 21:35:42 +0530

    Update Login.js

--- web-app/src/components/Login/Login.js
+++ web-app/src/components/Login/Login.js
@@ -4,6 +4,7 @@ import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { AuthContext } from '../../context/AuthContext';
 import { useContext } from "react";
 import { useNavigate } from 'react-router-dom';
+import './LoginPage.css'
 
 function Login() {
     let navigate = useNavigate()
@@ -34,10 +35,32 @@ function Login() {
     }
 
     return (
-        <div className="Login">
-            <h1>Login Page</h1>
-            <button onClick={signInWithGoogle}>SignInWithGoogle</button>
-        </div>
+        <div class="login">
+  <div class="login-triangle"></div>
+  
+  <h2 class="login-header">Log in</h2>
+
+  <form class="login-container">
+    <p><input type="email" placeholder="Email"/></p>
+    <p><input type="password" placeholder="Password"/></p>
+    <p><input type="submit" value="Log in"/></p>
+  </form>
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/> 
+<link rel="stylesheet" href="login.css"/>
+<div class="g_body">
+            
+            <button class="g-button" onClick={signInWithGoogle}> 
+            <img class="g-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/157px-Google_%22G%22_Logo.svg.png" alt="Google Logo"/>
+            <p class="g-text">Sign in with Google</p>
+            </button>
+            
+            </div>
+    
+  
+        
+</div>
+        
+        
     )
 }
 
