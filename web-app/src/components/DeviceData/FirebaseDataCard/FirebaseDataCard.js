dd24c5038574bf5211d85741616972b8546df8e9
Alan <alanphilipose@gmail.com>
2022-07-29 13:52:09 +0530

    add toggle button

--- web-app/src/components/DeviceData/FirebaseDataCard/FirebaseDataCard.js
+++ web-app/src/components/DeviceData/FirebaseDataCard/FirebaseDataCard.js
@@ -1,6 +1,7 @@
 import { database } from '../../../firebase/firebase';
 import { useEffect, useState } from 'react';
 import { onValue, ref, runTransaction } from 'firebase/database';
+import ToggleButton from 'react-toggle-button'
 
 function FirebaseDataCard({device_id, sensor, unit}) {
     const [firebaseData, setFirebaseData] = useState();
@@ -15,7 +16,7 @@ function FirebaseDataCard({device_id, sensor, unit}) {
     });
 
     return (
-        <div className="DataCard">
+        <div className="DataCard" >
         { unit === "bool" ?
              firebaseData ? <p>true</p> : <p>false</p>
             : <p>{ firebaseData }{ unit }</p>
@@ -25,7 +26,8 @@ function FirebaseDataCard({device_id, sensor, unit}) {
 }
 
 function FirebaseToggleButton({device_id}) {
-    const [firebaseData, setFirebaseData] = useState();
+    const [startWaterPump, setStartWaterPump] = useState();
+    const [isPumpRunning, setIsPumpRunning] = useState();
 
     function handleToggleButton() {
         const pumpRef = ref(database, device_id + "/start_water_pump");
@@ -40,15 +42,34 @@ function FirebaseToggleButton({device_id}) {
         const queryData = ref(database,  device_id + "/start_water_pump");
         return onValue(queryData, (snapshot) => {
             if (snapshot.exists()) {
-                setFirebaseData(snapshot.val());
+                setStartWaterPump(snapshot.val());
+            }
+        });
+    });
+
+    useEffect(() => {
+        const queryData = ref(database,  device_id + "/is_pump_running");
+        return onValue(queryData, (snapshot) => {
+            if (snapshot.exists()) {
+                setIsPumpRunning(snapshot.val());
             }
         });
     });
 
     return (
-        <div className="DataCard">
-            <p><button style={{ backgroundColor:'transparent',  fontSize: "1em", fontWeight: 'bold', color: 'white'}} onClick={handleToggleButton}>Force start</button>
-             <span> {firebaseData ? "true" : "false"}</span></p>
+        <div className="DataCard" style={{ display: 'flex', justifyContent: 'center'}}>
+            <ToggleButton
+            colors={{
+                active: {
+                    base:  isPumpRunning ? 'rgb(0, 100, 0)' : 'rgb(255,69,0)',
+                },
+                inactive: {
+                    base:  isPumpRunning ? 'rgb(0, 100, 0)' : 'rgb(255,69,0)',
+                }
+            }}
+            style={{ marginTop: '0px'}}
+            value={startWaterPump}
+            onToggle={ handleToggleButton } />
         </div>
     )
 }
